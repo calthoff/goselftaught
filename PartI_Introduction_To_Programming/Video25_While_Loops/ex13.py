8079c88f35c2342e832132f98ce9be43a6a8ace6
cloudenthusiast <praveenn069@gmail.com>
2018-07-19 19:16:10 +0530

    Video25 file examples

---
+++ PartI_Introduction_To_Programming/Video25_While_Loops/ex13.py
@@ -0,0 +1,9 @@
+list1 = [1, 2, 3, 4]
+list2 = [5, 6, 7, 8]
+added = []
+
+for i in list1:
+	for j in list2:
+		added.append(i+j)
+
+print(added)
---
+++ PartI_Introduction_To_Programming/Video25_While_Loops/ex14.py
@@ -0,0 +1,3 @@
+while input('y or n?') != 'n':
+	for i in range(1,6):
+		print(i)
