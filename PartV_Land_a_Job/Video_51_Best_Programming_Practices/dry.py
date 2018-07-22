02a9602919eae33032c450aea119508ec925ffbe
cloudenthusiast <praveenn069@gmail.com>
2018-07-22 13:53:43 +0530

    PartV Video51 files

---
+++ PartV_Land_a_Job/Video_51_Best_Programming_Practices/dry.py
@@ -0,0 +1,8 @@
+for i in range(10):
+    print("foo")
+    
+for i in range(10):
+    print("bar")
+    
+for i in range(10):
+    print("none")
---
+++ PartV_Land_a_Job/Video_51_Best_Programming_Practices/dry3.py
@@ -0,0 +1,3 @@
+def print_string(a_string, x):
+    for i in range(x):
+        print(a_string)
